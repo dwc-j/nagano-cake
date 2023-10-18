e1c72e96a508ff406b08cd93c1b64eee75e6c7be
EC2 Default User <ec2-user@ip-172-31-11-12.ap-northeast-1.compute.internal>
2023-10-18 00:32:09 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -14,30 +14,35 @@ devise_for :admin, skip: [:registrations, :passwords] , controllers: {
   root :to => "public/homes#top"
   get "about" => "homes#about"
   resources :items, only: [:index, :show]
-  
-  namespace :public do
-    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
-    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
-    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+
+  # namespace :public do
+    resource :customers, only: [:show, :edit, :update] do
+      member do
+        get :unsubscribe
+        patch :withdraw
+      end
+    end
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+      collection do
+        delete :destroy_all
+      end
+    end
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post :confirm
+        get :complete
+      end
+    end
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-  end
 
 
   namespace :admin do
-    get "/admin" => "homes#top"
-
-
-  
-  namespace :admin do
-    get "/admin" => "homes#top"
-    resources :items
+    get "admin" => "homes#top"
+    resources :items, except: [:destroy]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
   end
-
-
-
 
 end
