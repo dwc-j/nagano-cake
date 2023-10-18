6042c09eae6ccbdd833729baa52db77db7ddb992
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-10-18 13:54:51 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,6 @@ class Order < ApplicationRecord
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :name, presence: true
-  validates :payment_method, presence: true
 
   # アソシエーション
   belongs_to :user
