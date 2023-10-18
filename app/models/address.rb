4fe6fc85f62985cc4876cfc3f234d39ac746aae4
EC2 Default User <ec2-user@ip-172-31-43-96.ap-northeast-1.compute.internal>
2023-10-18 05:26:11 +0000

    addressのバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,7 @@ class Address < ApplicationRecord
    # バリデーション
   validates :postal_code, presence: true
   validates :address, presence: true
+  validates :na, presence: true
 
   # アソシエーション
   belongs_to :user
