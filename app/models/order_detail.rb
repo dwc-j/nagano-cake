265f65f3c846e9bcb262626a596ae8202b739197
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-10-18 13:45:19 +0900

    Update order_detail.rb

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,7 +1,5 @@
 class OrderDetail < ApplicationRecord
   # バリデーション
-  validates :item_id, presence: true
-  validates :order_id, presence: true
   validates :amount, presence: true
   validates :price, presence: true
   validates :making_status, presence: true
