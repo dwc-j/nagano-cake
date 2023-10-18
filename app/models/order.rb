e2a55bc3df34ff82603ebde4890de319feedafec
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-10-18 13:50:07 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,8 @@
 class Order < ApplicationRecord
   # バリデーション
-  validates :total_payment, presence: true
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :name, presence: true
-  validates :shipping_fee, presence: true
-  validates :status, presence: true
   validates :payment_method, presence: true
 
   # アソシエーション
