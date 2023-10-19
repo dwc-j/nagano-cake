e1b40dc1b419dbe427f859e2d6d09e6cd534164d
EC2 Default User <ec2-user@ip-172-31-43-96.ap-northeast-1.compute.internal>
2023-10-19 08:50:26 +0000

    管理者側の顧客一覧画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
@@ -9,5 +10,20 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: '会員情報が正常に更新されました'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :status)
   end
 end
