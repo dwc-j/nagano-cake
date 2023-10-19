1df9a0c4fcd8c9f07581884b2f842e1bb6bd194e
EC2 Default User <ec2-user@ip-172-31-43-96.ap-northeast-1.compute.internal>
2023-10-19 09:55:23 +0000

    adminのordersとdetails

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,27 @@
 class Admin::OrdersController < ApplicationController
   def show
   end
+  
+  def index
+    @orders = Order.all.page(params[:page]).per(10)
+  end
 
   def update
+    if @order.update(order_params)
+      redirect_to order_path(@order), notice: '注文ステータスが正常に更新されました'
+    else
+      render :show
+    end
   end
+  
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+  
 end
