3bd97f65dd083017a47b3a269a29ba41e74829ba
EC2 Default User <ec2-user@ip-172-31-43-96.ap-northeast-1.compute.internal>
2023-10-20 08:33:42 +0000

    気にしないでください

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,6 @@ devise_for :admin, skip: [:registrations, :passwords] , controllers: {
   get "/about" => "public/homes#about"
 
   get "admin" => "admin/homes#top"
-  
-  resources :items, only: [:index, :show]
 
 
   namespace :public, path: '' do #URLにpublicが入らないようにしました。
