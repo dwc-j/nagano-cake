8d5740872ed5d53c05739599d9d7e5cf6aa7d177
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-10-21 10:37:43 +0000

    [fin]devise

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,19 @@
 class Public::CustomersController < ApplicationController
+
+  before_action :authenticate_customer!
+
   def show
-    @customer = customer.find(params[:id])
+    @customer = current_customer
   end
 
   def edit
-    @customer = customer.find(params[:id])
+    @customer = current_customer
   end
-  
+
   def update
-     @customer = Customer.find(params[:id])
+     @customer = current_customer
     if @customer.update(customer_params)
-      redirect_to customer_path(@customer) 
+      redirect_to public_customers_information_path(@customer)
     else
       render :edit
     end
@@ -26,11 +29,11 @@ class Public::CustomersController < ApplicationController
     reset_session
     redirect_to root_path
   end
-  
+
    private
 
-  def costomer_params
+  def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
   end
-  
+
 end
