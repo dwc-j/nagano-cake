718febc2f3d0a8498f4f1bdcc54945d8c5719bd3
EC2 Default User <ec2-user@ip-172-31-11-12.ap-northeast-1.compute.internal>
2023-10-23 07:47:22 +0000

    Resolved merge conflict by incorporating both suggestions.

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,5 +3,10 @@ class OrderDetail < ApplicationRecord
 
   # アソシエーション
   belongs_to :order
-  belongs_to :product
+  belongs_to :item
+  
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+  
 end
