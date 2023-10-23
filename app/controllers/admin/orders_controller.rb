3092b51371c7f6ccf719f3cedb14759e24e2323e
EC2 Default User <ec2-user@ip-172-31-43-96.ap-northeast-1.compute.internal>
2023-10-23 10:10:26 +0000

    ステータスの更新

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -16,8 +16,9 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    @order = Order.find(params[:id])
     if @order.update(order_params)
-      redirect_to order_path(@order), notice: '注文ステータスが正常に更新されました'
+      redirect_to admin_order_path(@order), notice: '注文ステータスが正常に更新されました'
     else
       render :show
     end
