e09cc575df02f523010d707ad6805dbdbafe848a
EC2 Default User <ec2-user@ip-172-31-11-12.ap-northeast-1.compute.internal>
2023-10-23 10:12:52 +0000

    add current_customer

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,8 +3,9 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
   
   def update
+    @order_detail = OrderDetail.find(params[:id])
     if @order_detail.update(order_detail_params)
-      redirect_to order_path(@order_detail.order), notice: '制作ステータスが正常に更新されました'
+      redirect_to admin_order_path(@order_detail.order), notice: '制作ステータスが正常に更新されました'
     else
       render 'orders/show'
     end
