814f0671f5405a90f1e7a58ed98efccb3ed28847
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-10-24 10:45:19 +0000

    [fin]i18n

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,15 +9,14 @@ class Order < ApplicationRecord
   has_one_attached :image
 
     enum status: {
-    "waiting_for_payment":0,
-    "payment_confirmation":1,
-    "is_making":2,
-    "preparation_for_shipping":3,
-    "already_shipped":4
+    "waiting_for_payment": 0,
+    "payment_confirmation": 1,
+    "is_making": 2,
+    "preparation_for_shipping": 3,
+    "already_shipped": 4
   }
 
-  enum payment_method: ["クレジットカード", "銀行振込"]
-  # enum payment_method: { credit_card: 0, transfer: 1 }
+  enum payment_method: { credit_card: 0, transfer: 1 }
 
   def add_tax_price
     (self.price * 1.10).round
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -10,10 +10,10 @@ class OrderDetail < ApplicationRecord
   end
   
     enum making_status: {
-     "unable":0,
-     "waiting":1,
-     "making":2,
-     "completed":3,
+     "unable": 0,
+     "waiting": 1,
+     "making": 2,
+     "completed": 3,
   }
   
 end
