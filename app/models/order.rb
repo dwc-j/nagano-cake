776a97e813e2af489961205cc47d9b38cb4db678
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-10-24 05:18:26 +0000

    [fix]order_details_translation_missing

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,19 +4,20 @@ class Order < ApplicationRecord
   validates :address, presence: true
   validates :name, presence: true
 
-  # アソシエーション
   belongs_to :customer
   has_many :order_details, dependent: :destroy
   has_one_attached :image
 
-  enum status: {
-     "入金待ち":0,
-     "入金確認":1,
-     "製作中":2,
-     "発送準備中":3,
-     "発送済み":4
+    enum status: {
+    "waiting_for_payment":0,
+    "payment_confirmation":1,
+    "is_making":2,
+    "preparation_for_shipping":3,
+    "already_shipped":4
   }
+
   enum payment_method: ["クレジットカード", "銀行振込"]
+  # enum payment_method: { credit_card: 0, transfer: 1 }
 
   def add_tax_price
     (self.price * 1.10).round
