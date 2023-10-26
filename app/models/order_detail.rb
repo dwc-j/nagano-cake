0852a9b7d3eacd60b2f9fa687e6b62cf6c2f67b8
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-10-26 06:29:24 +0000

    [fix]

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -16,4 +16,18 @@ class OrderDetail < ApplicationRecord
      "completed": 3,
   }
   
-end
+  after_update :update_order_status, if: :saved_change_to_making_status?
+
+   private
+  
+   def update_order_status
+    if self.making_status == "making"
+     self.order.update(status: "is_making")
+    elsif self.making_status == "completed"
+    if self.order.order_details.all? { |detail| detail.making_status == "completed" }
+     self.order.update(status: "preparation_for_shipping")
+    end
+    end
+   end
+  
+  end
