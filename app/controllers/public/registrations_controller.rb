8064dae92c0b455198d9309a6ff6af3b7f311d7f
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-10-26 06:04:40 +0000

    [fix]after_sign_up_path

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,5 +2,8 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
 
+  def after_sign_in_path_for(resource)
+    public_customers_information_path(current_customer)
+  end
 
 end
